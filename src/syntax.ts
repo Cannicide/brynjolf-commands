72e1cf70d72a728fb6c49e3bd9c18b82f4dffd4f
Cannicide <41762964+Cannicide@users.noreply.github.com>
2023-12-10 03:09:47 -0500

    Added syntax parsing

---
+++ src/syntax.ts
@@ -0,0 +1,60 @@
+
+import { ApplicationCommandOptionType } from "discord-api-types/v10";
+import { BrynjolfArgumentTranslator, ResultOptions } from "./opts.js";
+
+// Handle syntax such as:
+// `${subcommand} <${arg1}> [${arg2}]`
+
+export default function parse(strings: TemplateStringsArray, ...values: BrynjolfArgumentTranslator[]): ResultOptions[] {
+    const args: ResultOptions[] = [];
+    let addToSubgroup = false;
+    let addToSubcommand = false;
+
+    for (const str of strings) {
+        const parts = str.split(/\s+/);
+
+        const classifier = parts.at(-1);
+        if (classifier === undefined) continue; 
+
+        const isSub = classifier.length == 0;
+        const isRequired = classifier == "<";
+        
+        const rawOpts = values.shift();
+        if (rawOpts === undefined) continue;
+        const opts = rawOpts._translate();
+
+        // Subgroup/subcommand functionality:
+        if (isSub) {
+            // If subgroup, append to args; toggle addToSubgroup
+            if (opts.type == ApplicationCommandOptionType.SubcommandGroup) {
+                args.push(opts);
+                addToSubgroup = true;
+            }
+            else if (opts.type == ApplicationCommandOptionType.Subcommand) {
+                // If subcommand, and addToSubgroup toggled, append to args and add to subgroup options; toggle addToSubcommand and untoggle addToSubgroup
+                if (addToSubgroup) {
+                    const subgroup = args[0];
+                    if (!subgroup?.options) subgroup!.options = [];
+                    subgroup!.options.push(opts);
+                    addToSubgroup = false;
+                }
+
+                args.push(opts);
+                addToSubcommand = true;
+            }
+        }
+        else {
+            if (isRequired) opts.required ??= true;
+            else opts.required ??= false;
+
+            if (addToSubcommand) {
+                const subcmd = args.at(-1);
+                if (!subcmd?.options) subcmd!.options = []; 
+                subcmd!.options.push(opts);
+            }
+            else args.push(opts);
+        }
+    }
+
+    return args;
+}
